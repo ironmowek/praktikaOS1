ec1ace6a97c3b36ecf07f48fa81f5d0399aa088c
mowek <negovorasasha@mail.ru>
2022-03-17 14:18:40 +0300

    End

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+
+    int Number;
+    Number = 0;
+    Console.WriteLine("Введите число: ");
+    Number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Вы ввели Number: {0}", Number);
+
+    //if (Number == 1)
+
+
+namespace HelloApp
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем диска: {drive.TotalSize}");
+                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+ 
